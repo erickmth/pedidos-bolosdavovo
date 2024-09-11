24899ce7bec7829c7f26390119f814138a273fea
erickmth <160558291+erickmth@users.noreply.github.com>
2024-09-10 23:26:10 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 // api/index.js
 const express = require('express');
-const path = require('path'); // Importa o módulo path para servir arquivos estáticos
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -15,14 +15,14 @@ app.use(express.json());
 
 // Rota para receber um pedido (POST /pedido)
 app.post('/pedido', (req, res) => {
-  const pedido = req.body;  // Recebe o pedido do frontend
-  pedidos.push(pedido);  // Adiciona o pedido na lista de pedidos
-  res.status(201).send('Pedido recebido com sucesso!');  // Responde com uma mensagem de sucesso
+  const pedido = req.body;
+  pedidos.push(pedido);
+  res.status(201).send('Pedido recebido com sucesso!');
 });
 
 // Rota para listar todos os pedidos (GET /pedidos)
 app.get('/pedidos', (req, res) => {
-  res.json(pedidos);  // Retorna todos os pedidos
+  res.json(pedidos);
 });
 
 app.listen(PORT, () => {
